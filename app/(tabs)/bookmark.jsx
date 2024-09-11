85fbe786409fe7f93a6709ad532e5f05e3759bc3
hypergalois <jose.delgado2@live.u-tad.com>
2024-09-11 17:02:55 +0200

    Create Upload screen UI

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
-import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Bookmark = () => {
-	return (
-		<View>
-			<Text>Bookmark</Text>
-		</View>
-	);
+    return (
+        <SafeAreaView className="bg-primary h-full">
+            <Text>Bookmark</Text>
+        </SafeAreaView>
+    );
 };
 
 export default Bookmark;
